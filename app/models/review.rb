bc3e43846ff5a3cdf6c27f7228151fbb83be8b28
JonCabot <jfc0053@gmail.com>
2022-04-12 20:59:07 -0500

    commit tree 9003e548714489567687d5c0eb1b372c5615b2ab

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
 class Review < ActiveRecord::Base
-  
+  # #a review belongs to a game
+  # def game
+  #   #self is the current/invoked review instance
+  #   Game.find(self.game_id)
+  # end
+  #=======that is the long-handed way, ActiveRecord macros do all this coding for us:
+  belongs_to :game
+
+  #it could look like this too: belongs_to(:game)    where pass in the argument of "game" as a symbol
+  #the belongs_to must be singular
 end
