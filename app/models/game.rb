9854915a1dbd9f44b536cc00276dbc345a5b58c0
JonCabot <jfc0053@gmail.com>
2022-04-12 21:00:14 -0500

    completed assignment

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,9 @@
 class Game < ActiveRecord::Base
+  has_many :reviews #plural
+  #this produces all of the reviews o nthis game (seraching by game_id; these are the results of the query)
   
+  #if we wrote this instance out in long-hand it would look like this:
+  # def reviews
+  #   Reviews.where(game_id: self.id)
+  # end
 end
